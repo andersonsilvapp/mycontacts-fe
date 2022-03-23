64f31431cf7dfc57bbf6fc5b52ed90ad90b58c80
Anderson Apparecido <anderson@bemobile.tech>
2022-03-22 22:54:17 -0300

    contacts list header component created

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,12 @@
 export default {
-  backgroundColor: '#f6f5fc',
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#f6f5fc',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346fb',
+
+    },
+  },
 };
