2f42f9c2204fdd652332d25a90f878e6d9b317f2
Anderson Apparecido <anderson@bemobile.tech>
2022-05-09 16:52:48 -0300

    feat: home page created and header refactor

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,92 @@
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListContainer,
+  Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import trash from '../../assets/images/icons/trash.svg';
+import edit from '../../assets/images/icons/edit.svg';
+
 export default function Home() {
-  return 'HomePage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar pelo nome" />
+      </InputSearchContainer>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Anderson Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>anderson@gmail.com</span>
+            <span>(12)9999-6999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Anderson Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>anderson@gmail.com</span>
+            <span>(12)9999-6999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Anderson Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>anderson@gmail.com</span>
+            <span>(12)9999-6999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,10 +4,32 @@ export const Container = styled.div`
   margin-top: 32px;
 `;
 
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+
+    outline: 0;
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #bcbcbc;
+    }
+  }
+`;
+
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;
@@ -52,7 +74,7 @@ export const ListContainer = styled.div`
 
 export const Card = styled.div`
   background: #fff;
-  box-shadow: 8px 4px 10px rgba(0,0,0,0.04);
+  box-shadow: 8px 4px 10px rgba(0, 0, 0, 0.04);
   padding: 16px;
   border-radius: 4px;
   display: flex;
@@ -82,7 +104,7 @@ export const Card = styled.div`
     span {
       display: block;
       font-size: 14px;
-      color: ${({ theme }) => theme.colors.gray[200]}
+      color: ${({ theme }) => theme.colors.gray[200]};
     }
   }
 
