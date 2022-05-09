3ec41aef113fc6ec1b94cdb02673c74169274e08
Anderson Apparecido <anderson@bemobile.tech>
2022-05-09 17:32:25 -0300

    feat: input and select component created

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,16 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="Novo contato" />;
+  return (
+    <>
+      <PageHeader title="Novo contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
