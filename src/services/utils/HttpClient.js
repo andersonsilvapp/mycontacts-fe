f718ec4c72acb798768c3cefcdf32f9ac5d2b330
Anderson Apparecido <anderson@bemobile.tech>
2022-06-06 17:51:43 -0300

    fix: request header content type added

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -26,6 +26,10 @@ class HttpClient {
 
     const headers = new Headers();
 
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
     if (options.headers) {
       Object.entries(options.headers).forEach(([name, value]) => {
         headers.append(name, value);
