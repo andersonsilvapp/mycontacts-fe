12ced40b2b68c6cb055d1b219913f9449ed797b3
Anderson <andersonsilvapp@gmail.com>
2022-06-23 23:03:16 -0300

    feat: event manager implemented

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,36 @@
+import { useState, useEffect } from 'react';
 import ToastMessage from '../ToastMessage';
 
 import { Container } from './styles';
 
+import { toastEventManager } from '../../../utils/toast';
+
 export default function ToastContainer() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast({ type, text }) {
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), type, text },
+      ]);
+    }
+    toastEventManager.on('addtoast', handleAddToast);
+
+    return () => {
+      toastEventManager.removeListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
